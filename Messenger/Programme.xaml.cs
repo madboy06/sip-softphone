cd62ece948c136eb188acb42f3167c3c81e09e83
vf <vf_info@mail.ru>
2012-05-10 17:51:48 +0000

    pl-PL localization
    git-svn-id: svn://svn.code.sf.net/p/officesip/code/trunk@40 db766ae0-df4e-4a0a-ba8f-e11ddc819d31

--- Messenger/Programme.xaml.cs
+++ Messenger/Programme.xaml.cs
@@ -32,7 +32,7 @@ namespace Messenger
 
 		public Programme()
 		{
-			//var cultureInfo = new System.Globalization.CultureInfo("tr-TR");
+			//var cultureInfo = new System.Globalization.CultureInfo("pl-PL");
 			//Thread.CurrentThread.CurrentCulture = cultureInfo;
 			//Thread.CurrentThread.CurrentUICulture = cultureInfo;
 #if DEBUG
--- Messenger/Properties/AssemblyInfoConsts.cs
+++ Messenger/Properties/AssemblyInfoConsts.cs
@@ -14,8 +14,8 @@ namespace Messenger
         public const string AssemblyProduct = "OfficeSIP Messenger";
         public const string AssemblyCopyright = "Copyright © 2008-2012 OfficeSIP Communications";
         public const string AssemblyTrademark = "OfficeSIP";
-        public const string AssemblyVersion = "2.2.5";
-        public const string AssemblyFileVersion = "2.2.5";
+        public const string AssemblyVersion = "2.2.7";
+        public const string AssemblyFileVersion = "2.2.7";
 
         public const string Homepage = "http://www.officesip.com";
 		public const string Feedback = "http://www.officesip.com/feedback.html";
